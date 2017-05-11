49240290b4cb89c91eab74e9ec68726fb069a838
SinzianaS <succubys@yahoo.com>
2017-05-11 15:58:41 +0300

    Initial commit

---
+++ play.js
@@ -0,0 +1,13 @@
+function Person(name, age, location){
+	this.name = name
+	this.age = age
+	this.location = location
+	walk(){
+		x: 0
+		y:0
+	}
+}
+
+let char1 = new Person("Ann", "20", "Cluj")
+char1.walk()
+
